841deea842e9e5553781afdf847b80b38eb9ec92
Apalog <gaetane.jallais@isen-lille.fr>
2017-06-09 16:49:02 +0200

    checkHour

---
+++ JarvisBis/Interface/js/chechHour.js
@@ -0,0 +1,22 @@
+// vérifier si il n'y a pas d'évenement à cette heure la et cette date la
+  // -> si oui evenement constellation
+  // si non -> ba les couilles
+
+var annee   = now.getFullYear();
+var mois    = now.getMonth() + 1;
+var jour    = now.getDate();
+var heure   = now.getHours();
+var minute  = now.getMinutes();
+var seconde = now.getSeconds();
+
+function check(string day){
+  if()
+
+
+
+
+}
+
+function valid(){
+
+}
