330226f00f3483a06d0181143c281d3907be2a88
Apalog <gaetane.jallais@isen-lille.fr>
2017-06-12 15:04:23 +0200

    controle event

--- JarvisBis/Interface/js/chechHour.js
+++ JarvisBis/Interface/js/chechHour.js
@@ -1,22 +1,43 @@
 // vérifier si il n'y a pas d'évenement à cette heure la et cette date la
   // -> si oui evenement constellation
   // si non -> ba les couilles
+var now = new Date();
+var trueYears = now.getFullYear();
+var trueMonth = now.getMonth() + 1;
+var trueDays  = now.getDate();
+var trueHour  = now.getHours();
+var trueMinute  = now.getMinutes();
+var trueSeconde = now.getSeconds();
+var years,month,days,hour,minute,seconde;
+//var requirejs = require('requirejs');
+//requirejs.config({/*your config with shims,etc goes here*/});
 
-var annee   = now.getFullYear();
-var mois    = now.getMonth() + 1;
-var jour    = now.getDate();
-var heure   = now.getHours();
-var minute  = now.getMinutes();
-var seconde = now.getSeconds();
+//var events = require("js/tableOfValue.js");
+window.onload = check;
 
-function check(string day){
-  if()
+console.log(test);
 
-
-
-
+function check(){
+  for(var i=0;i<events.length;i++ ){
+      var cut = events[i].date.split("-");
+      if(cut[0] == trueYears && cut[1] == trueMonth && cut[0] == trueDays){
+        var hour = events.Heure.split(":");
+        if(hour[0] == trueHour && hour[1] == trueMinute){
+          valid(events[i]);
+        }
+      }
+    }
+    setTimeout("check()", 60000);
 }
+function valid(action){
 
-function valid(){
+}/*
 
-}
+function delete(){
+  for(var i=0;i<events.length;i++){
+    var cut = events[i].date.split("-");
+    if(cut[0] < trueYears && cut[1] < trueMonth && cut[2] < trueDays ){
+
+    }
+  }
+}*/
