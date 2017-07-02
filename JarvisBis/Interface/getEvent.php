ac78992e89dafc3a66525a0d2baacb2613250dc4
julierbllrt <julie.robillart@isen.yncrea.fr>
2017-07-02 22:47:08 +0200

    final maj

--- JarvisBis/Interface/getEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$file = "js/events.json";
-
-echo file_get_contents($file);
-
-
-// $data = json_decode(file_get_contents($file), true);
-// var_dump($data);
--- JarvisBis/SoFun/www/js/php/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-$data = $_POST['json'];
-
-file_put_contents($_POST['file'], json_encode($data));
-
-echo "Event removed";
